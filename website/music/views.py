569ce03771513d8c6fb946fc2b5ae217157ce856
mosaab <musab.ehab@gmail.com>
2017-07-08 03:25:49 +0200

    Add some html to index view

--- website/music/views.py
+++ website/music/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-
+from .models import Album
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>music app home page</h1>")
+    all_albums = Album.objects.all()
+    html = ''
+
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="'+ url +'">' +album.album_title + '</a><br>'
+    return HttpResponse(html)
 
 def detail(request,album_id):
     return HttpResponse("<h2>Details for Album id : "+str(album_id) + "</h2>")
