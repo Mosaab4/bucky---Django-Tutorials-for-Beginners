67b2ee4bfcc08751176c22b080a1c2cae34989f6
mosaab <musab.ehab@gmail.com>
2017-07-09 08:44:29 +0200

    Add Favorite option to detail.html and views

--- website/music/urls.py
+++ website/music/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<album_id>[0-9]+)/$', views.detail ,name='detail'),
     url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite ,name='favorite'),
-
+    
 ]
